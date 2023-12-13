b16da09704a99b341ffe7121dae7ddd4fe74b49f
xiaosheng <lpy666@88.com>
2023-12-13 21:49:48 +0800

    commit add: delete useless code

--- sqlTest-api/src/main/java/com/cf/sqlTest/api/controller/TestController.java
+++ sqlTest-api/src/main/java/com/cf/sqlTest/api/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.cf.sqlTest.api.controller;
 
-import com.cf.sqlTest.facade.facade.UipsqlTestFacade;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +13,9 @@ import javax.annotation.Resource;
 //@RestController
     @Controller
 public class TestController {
-    @Resource
-    private UipsqlTestFacade uipsqlTestFacade;
     /**
      * transnal
      */
-    @GetMapping("/trans")
-    public String trans(){
-        uipsqlTestFacade.testTrans();
-        return "200";
-    }
-
     @GetMapping("/a/{page}")
     public String page(@PathVariable("page") String page){
         return page;
--- sqlTest-facade/src/main/java/com/cf/sqlTest/facade/facade/UipsqlTestFacade.java
+++ sqlTest-facade/src/main/java/com/cf/sqlTest/facade/facade/UipsqlTestFacade.java
@@ -8,12 +8,7 @@ import com.cf.sqlTest.facade.result.Result;
  * @date 2023/8/23
  */
 public interface UipsqlTestFacade {
-	/**
-	 * 保存sqlTest车架号信息
-	 *
-	 * @param param
-	 */
-	Result saveDevice(SaveDeviceDTO param);
+
 
 	void testTrans();
 }
